32431bd8f6d831d29f32e273dc48ad19a952b36b
99050630 <99050630@mydavinci.nl>
2022-02-14 14:35:32 +0100

    Update

--- functions/classes.php
+++ functions/classes.php
@@ -7,10 +7,51 @@
             $this->conn = $conn;
         }
         //Functions
+        //All remove functions
+        public function removeUser($boardId){
+            try{
+                $removeBoard = $this->conn->prepare("DELETE FROM bord WHERE id='".$boardId."'");
+                $removeBoardItems = $this->conn->prepare("DELETE FROM bord_items WHERE bord_id='".$boardId."'");
+
+                $removeBoard->execute();
+                $removeBoardItems->execute();
+
+                header("Location: index.php");
+            }catch(Exception $e){
+
+            }
+        }
+        //All update functions 
+        public function updateBoard($newBoardName, $boardId, $boardItems){
+            try{
+                $stmt = $this->conn->prepare("UPDATE bord SET `name`='".$newBoardName."' WHERE id='".$boardId."'");
+                $stmt->execute();
+                // print_r($boardItems);
+                $indexCount = 0;
+                for($i = 0; $i < count($boardItems); $i++){
+                    $getRowData = $this->conn->prepare("SELECT `id` FROM bord_items WHERE bord_id='".$boardId."' LIMIT 1 OFFSET ".$indexCount."");
+                    $getRowData->execute();
+                    $rowID = $getRowData->fetch();
+
+                    $updateRowData = $this->conn->prepare("UPDATE bord_items SET desc_1='".$boardItems[$i]."' WHERE id='".$rowID['id']."'");
+                    $updateRowData->execute();
+
+                    $indexCount++;
+                }
+
+                header("Location: index.php");
+            }catch(Exception $e){
+                echo 'Message: ' .$e->getMessage();
+            }
+        }
         //All load functions
-        public function loadBoard(){
+        public function loadBoard($id=""){
             $return_arr = array();
-            $stmt = $this->conn->prepare("SELECT * FROM bord WHERE `status`='1' ORDER BY `date` DESC");
+            $whereClause = "";
+            if($id != ""){
+                $whereClause = "AND id='".$id."'";
+            }
+            $stmt = $this->conn->prepare("SELECT * FROM bord WHERE `status`='1' $whereClause ORDER BY `date` DESC");
             $stmt->execute();
             $i = 0;
             while($result = $stmt->fetch()){
@@ -39,13 +80,23 @@
         }
         
         //All insert data functions
-        public function insertNewRow($boardID, $data){
+        public function insertNewRow($boardID, $data, $date){
             try{
-                $stmt = $this->conn->prepare("INSERT INTO bord_items (`bord_id`, `desc_1`, `status`) VALUES ('".$boardID."', '".$data."', '1')");
+                $stmt = $this->conn->prepare("INSERT INTO bord_items (`bord_id`, `desc_1`, `date`, `status`) VALUES ('".$boardID."', '".$data."', '".$date."', '1')");
                 $stmt->execute();
                 header("Location: index.php");
             } catch(Exception $e){
 
+            }
+        }
+
+        //make functions
+        public function makeBoard($name){
+            try{
+                $stmt = $this->conn->prepare("INSERT INTO bord (`name`, `date`, `status`) VALUES ('".$name."', '".date("Y-m-d")."', '1')");
+                $stmt->execute();
+                header("Location: index.php");
+            }catch(Exception $e){
             }
         }
     }
--- index.php
+++ index.php
@@ -3,9 +3,20 @@
     include "functions/classes.php";
 
     $todo = new toDoList($conn);
-
-    if(isset($_POST['addRowBtn']) && isset($_POST['addRow'])){
-        $todo->insertNewRow($_POST['board_id'], $_POST['addRow']);
+    //Remove content handelers
+    if(isset($_POST['boardRemove']) && isset($_POST['boardRemoveId']) && $_POST['boardRemoveId'] != ""){
+        $todo->removeUser($_POST['boardRemoveId']);
+    }
+    //Edit content handelers
+    if(isset($_POST['boardEditSave']) && isset($_POST['boardNameEdit']) && $_POST['boardNameEdit'] != '' && isset($_POST['listItemEdit'])){
+        $todo->updateBoard($_POST['boardNameEdit'], $_POST['boardIdEdit'], $_POST['listItemEdit']);
+    }
+    //Nieuwe content handelers
+    if(isset($_POST['addRowBtn']) && isset($_POST['addRow']) && isset($_POST['addTime'])){
+        $todo->insertNewRow($_POST['board_id'], $_POST['addRow'], $_POST['addTime']);
+    }
+    if(isset($_POST['newBoard']) && isset($_POST['boardName']) && $_POST['boardName'] != ""){
+        $todo->makeBoard($_POST['boardName']);
     }
 
 ?>
@@ -22,20 +33,82 @@
 <body>
     <div id="todolist">
         <div class="todolist_container">
-
+            <div class="todolist_header">
+                <?php 
+                    if(isset($_POST['boardEdit']) && isset($_POST['boardEditId']) && $_POST['boardEditId'] != ''){
+                        $editResult = $todo->loadBoard($_POST['boardEditId']);
+                        ?>
+                            <div class="todolist_header_title">
+                                <h2>Bord bewerken</h2>
+                            </div>
+                            <div class="todolist_content">
+                                <form method="POST">
+                                    <div class="edit_header">
+                                        <h2>Bord naam</h2>
+                                        <input type="hidden" name="boardIdEdit" value="<?php echo $editResult[0]['id']; ?>">
+                                        <input type="text" name="boardNameEdit" placeholder="Bijv. boodschappenlijst.." value="<?php echo $editResult[0]['name']; ?>">
+                                    </div>
+                                    <div class="edit_body">
+                                        <h2>Bord items</h2>
+                                    <?php
+                                        $getBordInfo = $todo->loadBoardInfo($editResult[0]['id']);
+                                        for($j = 0; $j < count($getBordInfo); $j++){
+                                            echo "<div class=\"list_item\">";
+                                            echo    "<input type='text' name='listItemEdit[]' value='".$getBordInfo[$j]['desc_1']."'>";
+                                            echo    "<input type=\"datetime-local\" name=\"listDateEdit[]\" value=\"".$getBordInfo[$i]['date']."\">";
+                                            echo    "<button type='submit' name='boardRemove'><i class=\"fa-solid fa-trash-can\"></i></button>";
+                                            echo "</div>";
+                                        }
+                                    ?>
+                                    <button type="submit" name="boardEditSave">Bewerken</button>
+                                    </div>
+                                </form>
+                            </div>
+                        <?php
+                    }else{
+                ?>
+                        <div class="todolist_header_title">
+                            <h2>Maak bord</h2>
+                        </div>
+                        <div class="todolist_content">
+                            <form method="POST">
+                                <input type="text" name="boardName" placeholder="Bijv. boodschappenlijst..">
+                                <button type="submit" name="newBoard">Aanmaken</button>
+                            </form>
+                        </div>
+                <?php 
+                    }
+                ?>
+            </div>
             <?php 
                 $result = $todo->loadBoard();
-                echo "<pre>";
-                print_r($result);
-                echo "</pre>";
+                // echo "<pre>";
+                // print_r($result);
+                // echo "</pre>";
                 for($i = 0; $i < count($result); $i++){
                     echo "<div class=\"list_container\" id=\"".$result[$i]['id']."\">";
-                    echo "  <div class=\"list_header\"><h2>".$result[$i]['name']."</h2><div class=\"list_header_right\"><p>".$result[$i]['date']."</p><i class=\"fa-solid fa-bars\"></i></div></div>";
+                    echo "  <div class=\"list_header\">";
+                    echo "      <h2>".$result[$i]['name']."</h2>";
+                    echo "      <div class=\"list_header_right\">";
+                    echo "          <p>".$result[$i]['date']."</p>";
+                    echo "          <form method='POST'>";
+                    echo "              <input type='hidden' name='boardEditId' value='".$result[$i]['id']."'>";
+                    echo "              <button type='submit' name='boardEdit'><i class=\"fa-solid fa-pencil\"></i></button>";
+                    echo "          </form>";
+                    echo "          <form method='POST'>";
+                    echo "              <input type='hidden' name='boardRemoveId' value='".$result[$i]['id']."'>";
+                    echo "              <button type='submit' name='boardRemove'><i class=\"fa-solid fa-trash-can\"></i></button>";
+                    echo "          </form>";
+                    echo "      </div>";
+                    echo "  </div>";
                     echo "  <div class=\"list_content\">";
                                 $getBordInfo = $todo->loadBoardInfo($result[$i]['id']);
                                 for($j = 0; $j < count($getBordInfo); $j++){
                                     echo "<div class=\"list_item\">";
-                                    echo    $getBordInfo[$j]['desc_1'];
+                                    echo    "<div>".$getBordInfo[$j]['desc_1']."</div>";
+                                    if($getBordInfo[$j]['date'] != "0000-00-00 00:00:00"){
+                                        echo    "<div>".$getBordInfo[$j]['date']."</div>";
+                                    }
                                     echo "</div>";
                                 }
                     echo "  </div>";
@@ -43,6 +116,7 @@
                     echo "      <form method=\"POST\">";
                     echo "          <input type='hidden' name='board_id' value=\"".$result[$i]['id']."\">";        
                     echo "          <input type='text' name='addRow' placeholder=\"Nieuwe regel toevoegen..\">";
+                    echo "          <input type='datetime-local' class=\"datetimeInput\" name='addTime'>";
                     echo "          <button type='submit' name=\"addRowBtn\">Toevoegen</button>";        
                     echo "      </form>";    
                     echo "  </div>";
